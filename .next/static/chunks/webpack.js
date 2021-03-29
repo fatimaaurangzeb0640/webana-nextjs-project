81f583af957d008f0945a1697aad24f57435ff6e
Samroon Michael <samroonmichael@gmail.com>
2021-03-30 06:27:17 +0800

    Refactor project structure & Add layout component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "90d8552ff5761bd99865";
+/******/ 	var hotCurrentHash = "097b00833237e5b2bdaf";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
