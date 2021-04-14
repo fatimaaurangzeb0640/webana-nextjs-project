c0f671beac3498dfa364ed03c9de007df617bdc9
unknown <fatimaaurangzeb0640@gmail.com>
2021-04-14 16:57:21 +0500

    porfolio animation not fully applied yet

--- src/components/Main-Components/PortfolioCard.js
+++ src/components/Main-Components/PortfolioCard.js
@@ -1,15 +1,27 @@
-import React from "react"
+import React, { useState } from 'react';
 /** @jsx jsx */
 import { css, jsx } from '@emotion/react'
 import styled from '@emotion/styled'
 import Link from "next/link"
 import Image from "next/image"
+import {MagnifyingGlass} from '@emotion-icons/entypo/MagnifyingGlass'
 
-const portfolioMain = css`text-align: center;
-                        background-image: url('/images/Background2.png');
-                        background-repeat: no-repeat;
-                        background-position: center;
-                        background-size: cover;`
+const MagnifyingGlassIcon = styled(MagnifyingGlass)`
+            color: white;
+            height: 1.5625rem;
+            width: 1.5625rem;
+            position: absolute;
+            top: 43%;
+            right: 45%;
+            left: 45%;
+            @media (max-width: 768px) {
+                height: 1.75rem;
+                width: 1.75rem;
+                right: 48%;
+                left: 48%;
+            }
+            /*border: 1px solid green;*/`
+
 
 const gridContainer = css`
                     /*border: 1px solid black;*/
@@ -32,15 +44,25 @@ const gridContainer = css`
                     }
 						`
 
+const gridItem = css`position: relative;
+                     /*border: 1px solid white;*/
+                     /*margin-right: 0.625rem;*/
+
+                    @media (min-width: 768px) {
+                        margin-right: 0.625rem;
+                    }
+                    @media (min-width: 1200px) {
+                            margin-right: 0.625rem;
+                    }`
+
 
 
 const Div = styled.div`
-                    /*border: 1px solid black;*/
+                    /*border: 1px solid red;*/
                     margin-bottom: 1.875rem; 
                     
-                    
                     @media (min-width: 768px) {
-                    margin-right: 0.625rem;
+                    /*margin-right: 0.625rem;*/
                     
                     /*margin-right: ${props => props.count==='5'? '20px' :'0625rem' };
                     margin-left: ${props => props.count==='4'? '70px' :'0px' };*/
@@ -48,11 +70,17 @@ const Div = styled.div`
                     }
 
                     @media (min-width: 1200px) {
-                    margin-right: 0.625rem;
+                    /*margin-right: 0.625rem;*/
                     }
                         `
+const img = css`    &:hover {
+                                opacity: 0.5;
+                        }` 
+
 	
 function PortfolioCard(props) {
+    const [isShown, setIsShown] = useState(false);
+
 	return (
         <div
         css={css`
@@ -74,26 +102,53 @@ function PortfolioCard(props) {
             </div>
 
             <div css={gridContainer}>  
-                <Div count='1'>
-                    <Image  src="/images/portfolio/Portfolio1.png" alt="Portfolio Image 1" width={260} height={410} />
-                </Div>
+                <div    css={gridItem}
+                        onMouseEnter={() => setIsShown(true)}
+                        onMouseLeave={() => setIsShown(false)}>
+                    <Div count='1'>
+                        <Image css={img}  src="/images/portfolio/Portfolio1.png" alt="Portfolio Image 1" width={260} height={410} />
+                    </Div>
+                    {isShown && (<MagnifyingGlassIcon></MagnifyingGlassIcon>)}
+                </div>
+              
+                <div    css={gridItem}
+                        onMouseEnter={() => setIsShown(true)}
+                        onMouseLeave={() => setIsShown(false)}>
+                    <Div count='2'>
+                        <Image css={img} src="/images/portfolio/Portfolio2.png" alt="Portfolio Image 2" width={260} height={410} />
+                    </Div>
+                    {isShown && (<MagnifyingGlassIcon></MagnifyingGlassIcon>)}
+                </div>
 
-                <Div count='2'>
-                    <Image  src="/images/portfolio/Portfolio2.png" alt="Portfolio Image 2" width={260} height={410} />
-                </Div>
+                <div    css={gridItem}
+                        onMouseEnter={() => setIsShown(true)}
+                        onMouseLeave={() => setIsShown(false)}>
+                    <Div count='3'>
+                        <Image css={img} src="/images/portfolio/Portfolio3.png" alt="Portfolio Image 3" width={260} height={410} />
+                    </Div>
+                    {isShown && (<MagnifyingGlassIcon></MagnifyingGlassIcon>)}
+                </div>
 
-                <Div count='3'>
-                    <Image  src="/images/portfolio/Portfolio3.png" alt="Portfolio Image 3" width={260} height={410} />
-                </Div>
+                <div    css={gridItem}
+                        onMouseEnter={() => setIsShown(true)}
+                        onMouseLeave={() => setIsShown(false)}>
+                    <Div count='4'>
+                        <Image css={img} src="/images/portfolio/Portfolio4.png" alt="Portfolio Image 4" width={260} height={410} />
+                    </Div>
+                    {isShown && (<MagnifyingGlassIcon></MagnifyingGlassIcon>)}
+                </div>
 
-                <Div count='4'>
-                    <Image  src="/images/portfolio/Portfolio4.png" alt="Portfolio Image 4" width={260} height={410} />
-                </Div>
-
-                <Div count='5'>
-                    <Image  src="/images/portfolio/Portfolio5.png" alt="Portfolio Image 5" width={260} height={410} />
-                </Div>
+                <div    css={gridItem}
+                        onMouseEnter={() => setIsShown(true)}
+                        onMouseLeave={() => setIsShown(false)}>
+                    <Div count='5'>
+                        <Image css={img}  src="/images/portfolio/Portfolio5.png" alt="Portfolio Image 5" width={260} height={410} />
+                    </Div>
+                    {isShown && (<MagnifyingGlassIcon></MagnifyingGlassIcon>)}
+                </div>
             </div>
+
+            
 
         </div>
 
